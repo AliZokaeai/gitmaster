a0848d8347311a1b2958dc0cb1420af0be98beaf
AliZokaea <alizokaeaidehaghani@gmail.com>
2020-03-21 10:55:02 +0430

    ali branches

---
+++ first.py
@@ -0,0 +1,16 @@
+print ('Ali you are perfect');
+
+Tuple = ("d", 12, "s34", "341")
+
+print(Tuple[2])
+
+print(Tuple[1:3])
+
+print(Tuple[1:6])
+print(Tuple[-1:-3])
+
+T3 = 30,
+print(type(T3))
+
+class1 = ((("m","3",3), "Math2", "Math3"), ("physics1", "Physice2"))
+print(len(class1[0][1]))
